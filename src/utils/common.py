f53129bb2fa636453ea797f79dfb3cca822f803c
Neelesh-Jaiswal <nikhilneelesh1@gmail.com>
2022-01-24 13:01:03 +0530

    commenting some lines in config

--- src/utils/common.py
+++ src/utils/common.py
@@ -1,10 +1,13 @@
 import yaml
 
+
 def read_config(config_path):
     with open(config_path) as config_file:
         content = yaml.safe_load(config_file)
-        print(content)
+        # print(content)
+        # print(content['params'])
     return content
+
 
 # if __name__ == '__main__':
 #     read_config('C:\\Users\Lenovo\Downloads\Ineuron\9th_Jan_2022_Perceptron\perceptron_implementation\config.yaml')
