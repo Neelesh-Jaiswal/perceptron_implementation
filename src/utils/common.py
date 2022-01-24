c67f7b740905197452f754b7da52f115ef96827f
Neelesh-Jaiswal <nikhilneelesh1@gmail.com>
2022-01-24 12:53:47 +0530

    adding calling method to read_config

--- src/utils/common.py
+++ src/utils/common.py
@@ -3,4 +3,8 @@ import yaml
 def read_config(config_path):
     with open(config_path) as config_file:
         content = yaml.safe_load(config_file)
+        print(content)
     return content
+
+# if __name__ == '__main__':
+#     read_config('C:\\Users\Lenovo\Downloads\Ineuron\9th_Jan_2022_Perceptron\perceptron_implementation\config.yaml')
